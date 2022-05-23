b4c39628294dbb28dbb0b0f48dbf3b9cf23a6588
marcelotores <marcelotores21@gmail.com>
2022-05-23 10:01:16 -0300

    tablespace 2

---
+++ postgres/tablespaces.sh
@@ -0,0 +1,17 @@
+### Tablespace
+
+# Tablespace permite que se defina locais no sistema de arquivos para representar os objetos do banco de dados
+# (banco de dados, tabelas ...). Ou seja, você armazena seus objetos em outros locais diferentes do cluster.
+# No entando aquele objeto ainda fará parte do cluster, de forma que metadados serão usados.
+# Isso serve caso algum servidor de banco fique sem espaço e não seja possível estender seu tamanho. Dessa forma 
+# pode-se criar um no spacetable até mesmo em outro disco diferente.
+#
+ 
+# exemplo de uso
+
+CREATE TABLESPACE fastspace LOCATION '/ssd1/postgresql/data';
+
+# O tablespace acima é criado em '/ssd1/postgresql/data' que já deve ter sido criado anteriormente no sistema
+# de arquivos
+
+
